be4b1920274e3618eb965f17ea8bd929f362a898
kenneth <johnkennethgibasabella@gmail.com>
2018-01-09 11:36:18 +0800

    removed unnecessary codes from templates/logs

--- src/assets/js/modules/Suppliers/Templates/Logs/Logs.js
+++ src/assets/js/modules/Suppliers/Templates/Logs/Logs.js
@@ -2,6 +2,7 @@ export default class{
 	constructor(){}
 
 	_template(){
+		//change badge color depending on the event type
 		let badgeClass='danger'
 
 		if(this.properties.event=="account"){
@@ -10,9 +11,6 @@ export default class{
 
 		//create element
 		this.element=document.createElement('div');
-
-		//set HTML content
-		//this.element_content=document.createElement('div');
 		
 		//shorten description
 		this.properties.description=this.properties.description.length>200?this.properties.description.substr(0,200)+'. . .':this.properties.description;
@@ -23,23 +21,8 @@ export default class{
 		this.element.innerHTML=`
 			<p class="logs"><span class="text-danger">[${this.properties.date}]</span> <span class="text-muted">${this.properties.description}</span> <span class="badge badge-${badgeClass}">${this.properties.event}</span></p>`
 
-        //this.element.appendChild(this.element_content)
-
         return this;
 	}
-
-	_bind(){
-
-		//save callback to windows
-		window.bms=window.bms||{}
-		window.bms.callback=window.bms.callback||{}
-
-		for(let event in this.properties.events){
-			this.element.addEventListener(event,this.properties.events[event])
-		}
-		
-	}
-
 
 	render(properties){
 		this.properties=properties||{}
@@ -48,10 +31,7 @@ export default class{
 
 		//render template
 		this._template()
-		this._bind();
 		return this.element;
 	}
-
-	dump(){ return this	}
 	
 }
