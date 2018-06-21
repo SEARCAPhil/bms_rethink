1f64b66980eade37b22f979350c6576e0d120295
kenneth <johnkennethgibasabella@gmail.com>
2018-06-21 09:40:59 +0800

    Changed JS  sdk from msal to adal

--- src/assets/js/modules/Authentication/Office365Auth.js
+++ src/assets/js/modules/Authentication/Office365Auth.js
@@ -1,3 +1,4 @@
+import AuthenticationContext from 'adal-angular';
 /*----------------------------------------------------------------------------------------------------------
 | MSAL script
 | This script is for authenticating user with microsoft AD and office365 ADVV2
@@ -12,7 +13,9 @@
 
 (function(window,document,undefined){
     "use strict";
-
+    // XHR
+    const XHR = new window.bms.exports.XHR()
+    // phonegap settings
     if (document.URL.indexOf( 'http://' ) === -1  && document.URL.indexOf( 'https://' ) === -1) {
        delete window.open;  // This restores the default browser
     }
@@ -23,121 +26,117 @@
         class:'spinner'
     })
 
-    var applicationConfig = {
-        clientID: 'dd7a26d3-c6ef-4aba-8ed4-b79688983849',
-        graphEndpoint: "https://graph.microsoft.com/beta/me/",
-        graphScopes: ["user.read"]
-    };
-
-     const XHR=new window.bms.exports.XHR()
-
-    var logger = new Msal.Logger(loggerCallback, { level: Msal.LogLevel.Verbose });
-
-    function loggerCallback(logLevel, message, piiEnabled) {
-        console.log(message);
+    // adal
+    window.adalConfig  = {
+        instance: 'https://login.microsoftonline.com/', 
+        tenant: 'common', //COMMON OR YOUR TENANT ID
+        clientId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', //This is your client ID
+        redirectUri: `http://localhost/bms_rethink/www/pages/`, //This is your redirect URI
+        cacheLocation: 'localStorage',
+        callback: userSignedIn,
+        popUp: true,
+        endpoints : {"https://graph.microsoft.com": "https://graph.microsoft.com"},
     }
-    
-    var userAgentApplication = new Msal.UserAgentApplication(applicationConfig.clientID, null, authCallback, { cacheLocation: 'localStorage', logger: logger });// cacheLocation defaults to sessionStorage if not set in the constructor
+    // adal global instance
+    window.AuthenticationContext = AuthenticationContext
 
-    function authCallback(errorDesc, token, error, tokenType) {
-        //This function is called after loginRedirect and acquireTokenRedirect. Use tokenType to determine context. 
-        //For loginRedirect, tokenType = "id_token". For acquireTokenRedirect, tokenType:"access_token".
-
-        if (token) {
-            userAgentApplication.acquireTokenSilent(applicationConfig.graphScopes).then(function (accessToken) {
-                // Change button to Sign Out
-                XHR.request({url:'https://graph.microsoft.com/beta/me/',method:'GET',headers:{'Authorization':'Bearer '+accessToken}}).then(json=>{
-                    let data=JSON.parse(json)
-                    if(data.id){
-                        // O365 check
-                        loginOnPremiseServer (data).then((json) => { 
-
-                            // account not yet verified
-                            if (!json.role) {
-                                window.location = 'authentication/confirmation.html'
-                                return 0
-                            }
-                            
-                            window.bms.default.spinner.hide()
-
-                            localStorage.setItem('token', json.token)
-                            localStorage.setItem('role', json.role)
-                            localStorage.setItem('username', data.mail)
-                            window.localStorage.setItem('id', data.id)
-                            window.localStorage.setItem('givenName', data.displayName)
-                            window.localStorage.setItem('department', data.department)
-                            window.localStorage.setItem('position', data.jobTitle)
-
-                            window.location = '../#/home/'
-                           
-                        }).catch((err) => {
-                            alert('Unable to authenticate. Please try again later.')
-                            window.bms.default.spinner.hide()
-                            document.querySelector('.btn-office365').removeAttribute('disabled')
-                        })
-                    }
-                })
-            }, function (error) {
-                console.log(error);
-                userAgentApplication.acquireTokenPopup(applicationConfig.graphScopes).then(function (accessToken) {
-                    console.log(userAgentApplication.getUser())
-                }, function (error) {
-                    console.log(error);
-                });
-            });
-        }
-        else if (errorDesc || error) {
-            console.log(error + ':' + errorDesc);
-        }
+    // adal error
+    const authError = () => {
+        alert('Unable to authenticate. Please try again later.')
     }
 
+    const loginOnPremiseServer = (data) => {
+        return new Promise((resolve, reject) => {
+            const XHR=new window.bms.exports.XHR()
+            const payload = {
+                url:`${window.bms.config.network}/auth/`,
+                method:'POST',
+                body: JSON.stringify({
+                    data,
+                }),
+            }
 
-    function loginRedirect() {
-        window.bms.default.spinner.show()
-        document.querySelector('.btn-office365').disabled = 'disabled'
-        userAgentApplication.loginRedirect(applicationConfig.graphScopes);
-    }
+            XHR.request(payload).then((json) => {
 
-    function logout() {
-        // Removes all sessions, need to call AAD endpoint to do full logout
-        userAgentApplication.logout();
-    }
-
-        function loginOnPremiseServer (data) {
-            return new Promise((resolve, reject) => {
-                const XHR=new window.bms.exports.XHR()
-                const payload = {
-                    url:`${window.bms.config.network}/auth/`,
-                    method:'POST',
-                    body: JSON.stringify({
-                        data,
-                    }),
-                }
-
-                XHR.request(payload).then((json) => {
-
-                    const data = JSON.parse(json)
-                    try{
-                        resolve(data) 
-                    }catch(err) {
-                        reject(err)
-                    }
-                   
-                })
+                const data = JSON.parse(json)
+                try{
+                    resolve(data) 
+                }catch(err) {
+                    reject(err)
+                } 
             })
+        })
+    }
 
+    // authenticate to remote server
+    const loginOnPremise = (data) => {
+        loginOnPremiseServer (data).then((json) => { 
+            // account not yet verified
+            if (!json.role) {
+                window.location = 'authentication/confirmation.html'
+                return 0
+            }
+            
+            window.bms.default.spinner.hide()
+
+            localStorage.setItem('token', json.token)
+            localStorage.setItem('role', json.role)
+            localStorage.setItem('username', data.mail)
+            window.localStorage.setItem('id', data.id)
+            window.localStorage.setItem('givenName', data.displayName)
+            window.localStorage.setItem('department', data.department)
+            window.localStorage.setItem('position', data.jobTitle)
+            window.location = '../#/home/'
+           
+        }).catch((err) => {
+            alert('Unable to authenticate. Please try again later.')
+            window.bms.default.spinner.hide()
+            authError()
+            document.querySelector('.btn-office365').removeAttribute('disabled')
+        })
+
+    }
+
+    // get msgraph
+    const getGraph = (token)  => {
+        fetch('https://graph.microsoft.com/beta/me/',{ 
+            headers:{'Authorization':'Bearer '+token}, 
+            method: 'GET'}
+        ).then(response => response.json()).catch((err) => {
+            authError()
+        }).then(data => {
+            // auth to onpremise
+            if(data.id) { 
+                loginOnPremise(data)
+            }
+        })
+    }
+
+    // adal callback
+    function userSignedIn(err, token) {
+        window.bms.default.spinner.show()
+        if (!err) {
+            window.ADAL.acquireToken("https://graph.microsoft.com", function (error, token) {
+                if(token.length) {
+                    getGraph(token)
+                } else {
+                    authError()
+                }
+            })
         }
+    }
 
-
-    
-
+    // adal instance
+    window.ADAL = new  window.AuthenticationContext(window.adalConfig);
+    window.ADAL.handleWindowCallback()
 
     //console
     console.log("%cUnauthorized Access","color: red; font-size: x-large")
     console.log("%cBidding Management System : You are trying to view the code in console","color: grey")
 
-    document.querySelector('.btn-office365').addEventListener('click',()=>{
-     loginRedirect()
+    document.querySelector('.btn-office365').addEventListener('click', (e) => {
+        e.target.disabled = 'disabled'
+        window.ADAL.login()
     })
 
 
